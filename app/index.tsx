70d6cd17ba870bbf047228994cdbbe3fd391d1e8
williams ayo <williamsayo44@gmail.com>
2026-05-04 22:18:25 +0300

    add HomeScreen component with themed container

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedContainer } from "@/components/themed/Container";
+import { Home } from "@/components/screens/home";
+
+export default function HomeScreen() {
+    return (
+        <ThemedContainer pi={6}>
+            <Home />
+        </ThemedContainer>
+    );
+}
