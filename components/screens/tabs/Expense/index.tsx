1e3242bc1c2791b07edd450cbe7854b5fe88b309
williams ayo <williamsayo44@gmail.com>
2026-05-09 12:03:52 +0300

    feat: add search input to Expense screen and refactor FlatList rendering

--- components/screens/tabs/Expense/index.tsx
+++ components/screens/tabs/Expense/index.tsx
@@ -1,8 +1,4 @@
-import {
-    BodyText,
-    CaptionText,
-    SubheaderText,
-} from "@/components/themed/Text";
+import { BodyText, CaptionText, SubheaderText } from "@/components/themed/Text";
 import { ThemedView } from "@/components/themed/View";
 import { FlatList } from "react-native";
 import { mixins } from "@/lib/tokens";
@@ -13,6 +9,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useExpenses } from "@/hooks/useExpense";
 import { formatDate, formatMoney } from "@/lib/utils/formatter";
 import { Spinner } from "@/components/ui/Spinner";
+import { ThemedTextInput } from "@/components/themed/Input/Text";
 
 export function Expense() {
     const { bottom } = useSafeAreaInsets();
@@ -27,57 +24,66 @@ export function Expense() {
     }
 
     return (
-        <FlatList
-            data={expenses}
-            keyExtractor={({ id }) => id}
-            ItemSeparatorComponent={() => (
-                <ThemedView height={1} bgColor="subtle" />
-            )}
-            ListHeaderComponent={() => (
-                <ThemedView borderBottomWidth={1} borderColor="subtle" pbe={4}>
-                    <SubheaderText size="2xl">All Expenses</SubheaderText>
-                </ThemedView>
-            )}
-            ListFooterComponent={() => (
-                <ThemedView height={BottomTabBarHeight - bottom} />
-            )}
-            contentContainerStyle={{ paddingHorizontal: Spacing[6] }}
-            showsVerticalScrollIndicator
-            renderItem={({ item }) => (
-                <ThemedView {...mixins.row} pb={4} gap={3}>
-                    <ThemedView bgColor="inverted" rounded="full" p={2}>
-                        <Icon
-                            name="receipt-outline"
-                            size={20}
-                            color="invertedText"
-                        />
+        <>
+            <ThemedView pi={6}>
+                <ThemedTextInput label="search" placeholder="search expense by name" hideLabel />
+            </ThemedView>
+            <FlatList
+                data={expenses}
+                keyExtractor={({ id }) => id}
+                ItemSeparatorComponent={() => (
+                    <ThemedView height={1} bgColor="subtle" />
+                )}
+                ListHeaderComponent={() => (
+                    <ThemedView
+                        borderBottomWidth={1}
+                        borderColor="subtle"
+                        pbe={4}
+                    >
+                        <SubheaderText size="2xl">All Expenses</SubheaderText>
                     </ThemedView>
-                    <ThemedView flex={1}>
-                        <BodyText size="sm">{item.name}</BodyText>
-                        <ThemedView {...mixins.row} gap={1} flexWrap="wrap">
-                            <CaptionText color="muted" size="xs">
-                                {formatDate(item.date)}
-                            </CaptionText>
-                            <CaptionText color="muted" size="xs">
-                                •
-                            </CaptionText>
-                            <ThemedView
-                                bgColor="subtle"
-                                rounded="full"
-                                pi={2.5}
-                                pb={0.5}
-                            >
-                                <CaptionText color="primary" size="xs">
-                                    {item.category}
+                )}
+                ListFooterComponent={() => (
+                    <ThemedView height={BottomTabBarHeight - bottom} />
+                )}
+                contentContainerStyle={{ paddingHorizontal: Spacing[6] }}
+                showsVerticalScrollIndicator
+                renderItem={({ item }) => (
+                    <ThemedView {...mixins.row} pb={4} gap={3}>
+                        <ThemedView bgColor="inverted" rounded="full" p={2}>
+                            <Icon
+                                name="receipt-outline"
+                                size={20}
+                                color="invertedText"
+                            />
+                        </ThemedView>
+                        <ThemedView flex={1}>
+                            <BodyText size="sm">{item.name}</BodyText>
+                            <ThemedView {...mixins.row} gap={1} flexWrap="wrap">
+                                <CaptionText color="muted" size="xs">
+                                    {formatDate(item.date)}
                                 </CaptionText>
+                                <CaptionText color="muted" size="xs">
+                                    •
+                                </CaptionText>
+                                <ThemedView
+                                    bgColor="subtle"
+                                    rounded="full"
+                                    pi={2.5}
+                                    pb={0.5}
+                                >
+                                    <CaptionText color="primary" size="xs">
+                                        {item.category}
+                                    </CaptionText>
+                                </ThemedView>
                             </ThemedView>
                         </ThemedView>
+                        <BodyText color="critical">
+                            -{formatMoney(item.amount, item.currency)}
+                        </BodyText>
                     </ThemedView>
-                    <BodyText color="critical">
-                        -{formatMoney(item.amount, item.currency)}
-                    </BodyText>
-                </ThemedView>
-            )}
-        />
+                )}
+            />
+        </>
     );
 }
