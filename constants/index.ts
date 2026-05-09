542d252f8e47b59bcc3c41898c533a33f44674bf
williams ayo <williamsayo44@gmail.com>
2026-05-09 12:11:37 +0300

    feat: simplify LOCAL_BACKEND_BASE_URL definition for consistency across platforms

--- constants/index.ts
+++ constants/index.ts
@@ -1,11 +1,6 @@
-import { Platform } from "react-native";
-
 export const BottomTabBarHeight = 70;
 
-const LOCAL_BACKEND_BASE_URL = Platform.select({
-    android: "http://192.168.137.1:8000/api/v1",
-    default: "http://192.168.137.1:8000/api/v1",
-});
+const LOCAL_BACKEND_BASE_URL = `http://localhost:8000/api/v1`;
 
 export const BACKEND_BASE_URL =
-	process.env.EXPO_PUBLIC_BACKEND_BASE_URL ?? LOCAL_BACKEND_BASE_URL;
+    process.env.EXPO_PUBLIC_BACKEND_BASE_URL ?? LOCAL_BACKEND_BASE_URL;
